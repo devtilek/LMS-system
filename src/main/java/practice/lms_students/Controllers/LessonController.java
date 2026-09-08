99f9cd15c920197bdea1a13ca1df6f69630b1218
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-08 18:19:57 +0500

    refactor: validate lessons and return proper status codes

--- src/main/java/practice/lms_students/Controllers/LessonController.java
+++ src/main/java/practice/lms_students/Controllers/LessonController.java
@@ -1,6 +1,8 @@
 package practice.lms_students.Controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import practice.lms_students.DTO.LessonDTO;
 import practice.lms_students.Service.LessonService;
@@ -14,17 +16,19 @@ public class LessonController {
     private final LessonService lessonService;
 
     @PostMapping
-    public LessonDTO createLesson(@RequestBody LessonDTO lessonDTO){
+    @ResponseStatus(HttpStatus.CREATED)
+    public LessonDTO createLesson(@Valid @RequestBody LessonDTO lessonDTO) {
         return lessonService.createLesson(lessonDTO);
     }
 
     @GetMapping("/course/{courseId}")
-    public List<LessonDTO> getById(@PathVariable Long courseId){
+    public List<LessonDTO> getByCourse(@PathVariable Long courseId) {
         return lessonService.getByCourse(courseId);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
         lessonService.deleteLesson(id);
     }
 }
