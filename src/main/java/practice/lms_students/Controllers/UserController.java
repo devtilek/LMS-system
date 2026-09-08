dae6a3be5fb0031c80672244defc330d1f4ba4ed
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-08 18:20:40 +0500

    refactor: validate users and use REST status codes

--- src/main/java/practice/lms_students/Controllers/UserController.java
+++ src/main/java/practice/lms_students/Controllers/UserController.java
@@ -1,8 +1,8 @@
 package practice.lms_students.Controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import practice.lms_students.DTO.UserRequestDTO;
 import practice.lms_students.DTO.UserResponseDTO;
@@ -17,22 +17,24 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDTO> register(@RequestBody UserRequestDTO userRequestDTO){
-        return new ResponseEntity<>(userService.register(userRequestDTO), HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponseDTO register(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+        return userService.register(userRequestDTO);
     }
 
-    @GetMapping()
-    public List<UserResponseDTO> getAll(){
+    @GetMapping
+    public List<UserResponseDTO> getAll() {
         return userService.getAll();
     }
 
     @GetMapping("/{id}")
-    public UserResponseDTO getById(@PathVariable Long id){
+    public UserResponseDTO getById(@PathVariable Long id) {
         return userService.getById(id);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
         userService.deleteById(id);
     }
 }
