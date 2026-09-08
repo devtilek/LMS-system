f5b928cf3bda655a5aa3fea8f57c94e112f92ef5
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-08 18:18:37 +0500

    refactor: query lessons by course in database

--- src/main/java/practice/lms_students/Repository/LessonRepo.java
+++ src/main/java/practice/lms_students/Repository/LessonRepo.java
@@ -2,10 +2,11 @@ package practice.lms_students.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import practice.lms_students.Entity.Lesson;
 
+import java.util.List;
+
 @Repository
-@Transactional
-public interface LessonRepo extends JpaRepository<Lesson, Long>{
+public interface LessonRepo extends JpaRepository<Lesson, Long> {
+    List<Lesson> findByCourseId(Long courseId);
 }
