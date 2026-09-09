4dba1f407215d6e62643671d5d04e8cdd4e14e98
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-09 20:05:50 +0500

    refactor: expose authenticated user profile instead of user administration

--- src/main/java/practice/lms_students/Controllers/UserController.java
+++ src/main/java/practice/lms_students/Controllers/UserController.java
@@ -3,12 +3,11 @@ package practice.lms_students.Controllers;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import practice.lms_students.DTO.UserRequestDTO;
 import practice.lms_students.DTO.UserResponseDTO;
 import practice.lms_students.Service.UserService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -22,19 +21,8 @@ public class UserController {
         return userService.register(userRequestDTO);
     }
 
-    @GetMapping
-    public List<UserResponseDTO> getAll() {
-        return userService.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public UserResponseDTO getById(@PathVariable Long id) {
-        return userService.getById(id);
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteById(@PathVariable Long id) {
-        userService.deleteById(id);
+    @GetMapping("/me")
+    public UserResponseDTO getCurrentUser(Authentication authentication) {
+        return userService.getCurrentUser(authentication);
     }
 }
